d7160265f0f6e29c36aae084dcc16a87b10ac531
Matt Johnson <mj1856@hotmail.com>
2013-07-16 17:13:35 -0700

    Update assemblyinfo

--- Raven.Bundles.IndexedAttachments.Tests/Properties/AssemblyInfo.cs
+++ Raven.Bundles.IndexedAttachments.Tests/Properties/AssemblyInfo.cs
@@ -1,36 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Raven.Bundles.IndexedAttachments.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Raven.Bundles.IndexedAttachments.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyDescription("Tests for the Indexed Attachments Bundle.")]
+[assembly: AssemblyCompany("Matt Johnson")]
+[assembly: AssemblyProduct("RavenDB Indexed Attachments Bundle")]
+[assembly: AssemblyCopyright("Copyright © 2012-2013 - Matt Johnson")]
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5c6139c6-b23d-49e7-86cb-3559e21fd9eb")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- Raven.Bundles.IndexedAttachments/Properties/AssemblyInfo.cs
+++ Raven.Bundles.IndexedAttachments/Properties/AssemblyInfo.cs
@@ -1,6 +1,11 @@
 ﻿using System.Reflection;
+using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Raven.Bundles.IndexedAttachments")]
 [assembly: AssemblyDescription("Plugin for RavenDB that extracts text from attachments so they can be indexed.")]
-[assembly: AssemblyVersion("3.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.8")]
+[assembly: AssemblyCompany("Matt Johnson")]
+[assembly: AssemblyProduct("RavenDB Indexed Attachments Bundle")]
+[assembly: AssemblyCopyright("Copyright © 2012-2013 - Matt Johnson")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("2.1.0")]
+[assembly: AssemblyFileVersion("2.1.0.8")]
