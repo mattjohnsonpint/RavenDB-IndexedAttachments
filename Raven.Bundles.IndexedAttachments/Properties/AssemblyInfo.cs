ee43dcf16ce92015af8652a903766319bf03bb47
Matt Johnson <mj1856@hotmail.com>
2013-07-16 18:17:42 -0700

    use 3-digit versioning

--- Raven.Bundles.IndexedAttachments/Properties/AssemblyInfo.cs
+++ Raven.Bundles.IndexedAttachments/Properties/AssemblyInfo.cs
@@ -9,3 +9,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("2.1.0")]
 [assembly: AssemblyFileVersion("2.1.0.8")]
+[assembly: AssemblyInformationalVersion("2.1.0")]
