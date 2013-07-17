19910ab0b8848a45c2e51ed38cb4eb43efd5ff23
Matt Johnson <mj1856@hotmail.com>
2013-07-16 17:01:11 -0700

    Migrated from raven.contrib

---
+++ Raven.Bundles.IndexedAttachments/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Raven.Bundles.IndexedAttachments")]
+[assembly: AssemblyDescription("Plugin for RavenDB that extracts text from attachments so they can be indexed.")]
+[assembly: AssemblyVersion("3.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.8")]
