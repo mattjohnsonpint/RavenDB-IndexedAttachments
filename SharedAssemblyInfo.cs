5fca2c82536b4a6e9ec6bdb0deb3a93693b8bde3
Matt Johnson <mj1856@hotmail.com>
2014-02-05 00:16:21 -0800

    Version Bump 2.5.5

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -3,8 +3,8 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Matt Johnson")]
 [assembly: AssemblyProduct("RavenDB Indexed Attachments Bundle")]
-[assembly: AssemblyCopyright("Copyright © 2012-2013 Matt Johnson")]
+[assembly: AssemblyCopyright("Copyright © 2012-2014 Matt Johnson")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.5.4")]
-[assembly: AssemblyFileVersion("2.5.4.11")]
-[assembly: AssemblyInformationalVersion("2.5.4")]
+[assembly: AssemblyVersion("2.5.5")]
+[assembly: AssemblyFileVersion("2.5.5.12")]
+[assembly: AssemblyInformationalVersion("2.5.5")]
